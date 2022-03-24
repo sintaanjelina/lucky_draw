16f2db896ad32870f62b9fdd6d7a508a673d1902
Sinta Anjelina <sinta.anjelina3930@gmail.com>
2022-03-24 17:10:13 +0700

    Register and Login

--- web-app/app.py
+++ web-app/app.py
@@ -1,12 +1,100 @@
-from flask import Flask, jsonify, request, render_template, send_file, make_response
+from flask import Flask, flash, redirect, url_for, jsonify, request, render_template, send_file, make_response
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import time
+import datetime
+
+from passlib.hash import sha256_crypt
+engine = create_engine("mysql+pymysql://root:@localhost/db_lucky_draw")
+# mysql+pymysql://username:password@localhost/databasename
+
+db = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
+app.secret_key = b'"F4Q8z\n\xec]/'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+# # Required Setup MYSQL data
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/db_lucky_draw'
+# db = SQLAlchemy(app)
+
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == "POST":
+        ts = time.time()
+        timestamp = datetime.datetime.fromtimestamp(
+            ts).strftime('%Y-%m-%d %H:%M:%S')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        secure_password = sha256_crypt.encrypt(str(password))
+
+        usernamedata = db.execute("SELECT username FROM tb_users WHERE username=:username", {
+                                  "username": username}).fetchone()
+        # usernamedata=str(usernamedata)
+        if usernamedata == None:
+            if password == confirm_password:
+                db.execute("INSERT INTO tb_users(username,password,register_date) VALUES(:username,:password,:register_date)",
+                           {"username": username, "password": secure_password, "register_date": timestamp})
+                db.commit()
+                flash("You are registered and can now login", "success")
+                return redirect(url_for('login'))
+            else:
+                flash("password does not match", "danger")
+                return render_template('register.html')
+        else:
+            flash("user already existed, please login or contact admin", "danger")
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(username)
+        print(password)
+        usernamedata = db.execute("SELECT username FROM tb_users WHERE username=:username", {
+                                  "username": username}).fetchone()
+        passworddata = db.execute("SELECT password FROM tb_users WHERE username=:username", {
+                                  "username": username}).fetchone()
+
+        print(usernamedata)
+        print(passworddata)
+        print(sha256_crypt.hash((request.form.get('password'))))
+        if usernamedata is None:
+            flash("No username", "danger")
+            return render_template("index.html")
+        else:
+            for passwor_data in passworddata:
+                print(password, passwor_data)
+                if sha256_crypt.verify(password, passwor_data):
+                    session = True
+                    flash("You are now logged in!!", "success")
+
+                    # to be edited from here do redict to either svm or home
+                    return redirect(url_for("home"))
+            else:
+                flash("incorrect password", "danger")
+                return render_template("index.html")
+
+    return render_template("index.html")
 
 
 @app.route('/')
 def index():
     path = request.path
     return render_template('index.html', data=path)
+
+
+@app.route('/daftar')
+def daftar():
+    path = request.path
+    return render_template('register.html', data=path)
 
 
 @app.route('/beranda')
---
+++ web-app/connect.py
@@ -0,0 +1,32 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+# Required Setup MYSQL data
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/db_lucky_draw'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+admin = User('admin', 'admin@example.com')
+
+db.create_all()  # In case user table doesn't exists already. Else remove it.
+
+db.session.add(admin)
+
+db.session.commit()  # This is needed to write the changes to database
+
+User.query.all()
+
+User.query.filter_by(username='admin').first()
